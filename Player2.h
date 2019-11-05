e1fb83cb563b52a9dc10b7420358e37c79a022be
Niko Kiviaho <43332074+NicotineDark@users.noreply.github.com>
2019-11-05 10:40:39 +0200

    Add files via upload

---
+++ Player2.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Player
+{
+// Class variables
+public:
+	string playerName;
+	int playerLevel;
+
+// Class methods
+	Player(string pName, int pLevel);
+	Player();
+	~Player();
+};
+
