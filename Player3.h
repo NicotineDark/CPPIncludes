1fef15e4a176aad171048501115952a639f267e7
Niko Kiviaho <43332074+NicotineDark@users.noreply.github.com>
2019-11-05 11:41:40 +0200

    Add files via upload

---
+++ Player3.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Player
+{
+// Class variables
+public:
+	string playerName;
+	int playerLevel;
+
+// Class methods
+	Player(string pName, int pLevel);
+	Player();
+	~Player();
+	void CopyPlayer(Player pPlayer);
+
+
+
+};
+
