ebd73b3a46f2f3f897d716b993219b880b88d4fc
Niko Kiviaho <43332074+NicotineDark@users.noreply.github.com>
2019-11-18 14:58:29 +0200

    Create GameBaseWeamor.h

---
+++ GameBaseWeamor.h
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+class Equipment
+{
+private:
+	string name;
+	double weight;
+
+public:
+	Equipment(string pName, double pWeight)
+	{
+		name = pName;
+		weight = pWeight;
+	}
+	Equipment()
+	{
+		name = "";
+		weight = 0.0;
+	}
+};
+
+class Weapon :
+	public Equipment
+{	
+private:
+	int damage;
+
+Weapon(string pName, double pWeight, int pDam) : 
+	Equipment(pName, pWeight)
+{
+	damage = pDam;
+}
+Weapon() : 
+	Equipment()
+{
+	damage = 0;
+}
+};
+
+class Armor :
+	public Equipment
+{
+private:
+	int protection;
+
+public:
+	Armor(string pName, double pWeight, int pProt)
+		: Equipment(pName, pWeight)
+	{
+		protection = pProt;
+	}
+	Armor()
+		: Equipment()
+	{
+		protection = 0;
+	}
+};
+class Weamor :
+	public Weapon,
+	public Armor
+{
+private:
+	int protection;
+
+public:
+	Weamor(string pName, double pWeight, int pDam, int pProt);
+	Weamor();
+};
