df8bb1bf0197adfb64dfc30472eeade7f1279201
Niko Kiviaho <43332074+NicotineDark@users.noreply.github.com>
2019-11-28 13:24:11 +0200

    Create PointerBaseClasses2.h

---
+++ PointerBaseClasses2.h
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+class Equipment
+{
+private:
+	string name;
+	double weight;
+
+public:
+	Equipment(string pName, double pWeight)
+	{
+		name = pName;
+		weight = pWeight;
+	}
+	Equipment()
+	{
+		name = "";
+		weight = 0.0;
+
+	}
+};
+class Armor :
+	public Equipment
+{
+private:
+	int protection;
+
+public:
+	Armor(string pName, double pWeight, int pProt)
+		: Equipment(pName, pWeight)
+	{
+		protection = pProt;
+	}
+	Armor()
+		: Equipment()
+	{
+		protection = 0;
+	}
+};
+class Weapon :
+	public Equipment
+{
+private:
+	int damage;
+
+public:
+	Weapon(string pName, double pWeight, int pDam)
+		: Equipment(pName, pWeight)
+	{
+		damage = pDam;
+	}
+	Weapon()
+		: Equipment()
+	{
+		damage = 0;
+	}
+};
+class Backpack
+{
+private:
+	Equipment* eqs;
+public:
+	Backpack();
+	Backpack(int pAmount);
+};
